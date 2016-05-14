b6a98aa934fecc3ebf224742f9a41aef34114cef
Sandi Andrian <andrian.sandi@gmail.com>
2016-05-14 21:41:01 +0700

    Initial Commit

---
+++ kdr-custom-form-shortcode.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: KDR Custom Form Shortcode
+ * Plugin URI: https://www.kodrindonesia.com
+ * Description: Provide you custom form tag html using shortcode in a nice ways!
+ * Version: 1.0.0
+ * Author: Kodr Indonesia
+ * Author URI: https://www.kodrindonesia.com
+ * License: GPL2
+ */
